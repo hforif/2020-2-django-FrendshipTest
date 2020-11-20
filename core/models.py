2fd685846de2498c499929125705658ced74a901
E-lutris <giwuni@naver.com>
2020-11-20 15:17:16 +0900

    new model Player,Master

--- core/models.py
+++ core/models.py
@@ -1,4 +1,43 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
+class Master(models.Model):
+    name = models.CharField(max_length=20)
+    choice1 = models.BooleanField(default=False)
+    choice2 = models.BooleanField(default=False)
+    choice3 = models.BooleanField(default=False)
+    choice4 = models.BooleanField(default=False)
+    choice5 = models.BooleanField(default=False)
+    choice6 = models.BooleanField(default=False)
+    choice7 = models.BooleanField(default=False)
+    choice8 = models.BooleanField(default=False)
+    choice9 = models.BooleanField(default=False)
+    choice10 = models.BooleanField(default=False)
+
+    # 왼쪽이 True 오른쪽이 False
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+    choice1 = models.BooleanField(default=False)
+    choice2 = models.BooleanField(default=False)
+    choice3 = models.BooleanField(default=False)
+    choice4 = models.BooleanField(default=False)
+    choice5 = models.BooleanField(default=False)
+    choice6 = models.BooleanField(default=False)
+    choice7 = models.BooleanField(default=False)
+    choice8 = models.BooleanField(default=False)
+    choice9 = models.BooleanField(default=False)
+    choice10 = models.BooleanField(default=False)
+
+    # 왼쪽이 True 오른쪽이 False
+
+    def __str__(self):
+        return self.name
